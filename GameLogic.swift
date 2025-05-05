20f16aa8eb494353b077ed2ce7cd05ae386d14b7
ATMaii <m_maaii@hotmail.com>
2025-05-06 03:12:33 +0700

    Update GameLogic.swift

--- GameLogic.swift
+++ GameLogic.swift
@@ -1,1 +1,30 @@
 
+// GameLogic.swift
+
+import Foundation
+
+class GameLogic {
+    var deck = Deck()
+    var players: [Player] = []
+
+    init(playerNames: [String]) {
+        self.players = playerNames.map { Player(name: $0) }
+    }
+
+    func startGame() {
+        deck.reset()
+        dealCards()
+    }
+
+    private func dealCards() {
+        for player in players {
+            player.hand = deck.drawCards(count: 13)
+            player.hand.sort {
+                if $0.rank.rawValue == $1.rank.rawValue {
+                    return $0.suit.rawValue < $1.suit.rawValue
+                }
+                return $0.rank.rawValue < $1.rank.rawValue
+            }
+        }
+    }
+}
