ec4996371d4597831737e6e503c58ff7683e67bb
ATMaii <m_maaii@hotmail.com>
2025-05-06 03:11:44 +0700

    Update Player.swift

--- Player.swift
+++ Player.swift
@@ -2,3 +2,18 @@
 struct Player {
     var hand: [Card] = []
 }
+
+// Player.swift
+
+import Foundation
+
+class Player: Identifiable {
+    let id = UUID()
+    var name: String
+    var hand: [Card]
+
+    init(name: String) {
+        self.name = name
+        self.hand = []
+    }
+}
