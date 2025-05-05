786f8c399a999dbd70b1b420036302921fc911d3
ATMaii <m_maaii@hotmail.com>
2025-05-06 03:11:12 +0700

    Update Deck.swift

--- Deck.swift
+++ Deck.swift
@@ -15,3 +15,44 @@ struct Deck {
         return cards.popLast()
     }
 }
+
+// Deck.swift
+
+import Foundation
+
+class Deck {
+    private(set) var cards: [Card] = []
+
+    init() {
+        reset()
+    }
+
+    func reset() {
+        cards = []
+        for suit in Suit.allCases {
+            for rank in Rank.allCases {
+                cards.append(Card(suit: suit, rank: rank))
+            }
+        }
+        shuffle()
+    }
+
+    func shuffle() {
+        cards.shuffle()
+    }
+
+    func drawCard() -> Card? {
+        guard !cards.isEmpty else { return nil }
+        return cards.removeFirst()
+    }
+
+    func drawCards(count: Int) -> [Card] {
+        var drawn: [Card] = []
+        for _ in 0..<count {
+            if let card = drawCard() {
+                drawn.append(card)
+            }
+        }
+        return drawn
+    }
+}
