e4a1cc34909c04b85b0ff2000759ccdfc42eb71d
ATMaii <m_maaii@hotmail.com>
2025-05-06 12:59:22 +0700

    Update GameView.swift
    Add

--- GameView.swift
+++ GameView.swift
@@ -1,4 +1,19 @@
 
+import SwiftUI
+
+struct GameView: View {
+    @State private var deck = Deck()
+    @State private var player = Player()
+    
+    var body: some View {
+        VStack {
+            Text("ไพ่ของผู้เล่น").font(.title)
+            
+            ScrollView(.horizontal) {
+                HStack {
+                    ForEach(player.hand.sorted(by: cardSort), id: \.self) { card in
+                        Text("
+                             
 // GameView.swift
 
 import SwiftUI
