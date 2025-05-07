a56b68df5541e1025b374fb745060b839e1e3e4b
ATMaii <m_maaii@hotmail.com>
2025-05-07 18:35:47 +0700

    Create Arena.swift

---
+++ Arena.swift
@@ -0,0 +1,10 @@
+// Arena.swift import Foundation
+
+struct Arena { let stadium: Stadium let roomValue: Int let playersJoined: Int let maxPlayers: Int
+
+var isFull: Bool {
+    playersJoined >= maxPlayers
+}
+
+var roomName: String {
+    "
