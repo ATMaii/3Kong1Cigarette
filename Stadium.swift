84b5f55c38019a80011671c33ad07c94de4fdd2a
ATMaii <m_maaii@hotmail.com>
2025-05-07 18:35:13 +0700

    Update Stadium.swift

--- Stadium.swift
+++ Stadium.swift
@@ -39,3 +39,34 @@ enum Stadium: String, CaseIterable {
         }
     }
 }
+// Stadium.swift import Foundation
+
+enum Stadium: String { case Rookie = "Wembley" case Beginner = "Maracana" case Amature = "Allianz Arena" case Master = "Santiago Bernabeu"
+
+var minChips: Int {
+    switch self {
+    case .Rookie: return 5_000
+    case .Beginner: return 10_000
+    case .Amature: return 20_000
+    case .Master: return 50_000
+    }
+}
+
+var multiplier: Double {
+    switch self {
+    case .Rookie: return 5.2
+    case .Beginner: return 6.3
+    case .Amature: return 7.4
+    case .Master: return 8.5
+    }
+}
+
+var availableRooms: [Int] {
+    switch self {
+    case .Rookie, .Beginner: return [50, 100, 200]
+    case .Amature: return [50, 100, 200, 500]
+    case .Master: return [100, 200, 500, 1000]
+    }
+}
+
+}
