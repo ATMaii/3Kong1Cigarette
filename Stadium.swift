10d2995d64dcbf337329ab29c9a59f062d1d3e09
ATMaii <m_maaii@hotmail.com>
2025-05-08 23:49:28 +0700

    Update Stadium.swift

--- Stadium.swift
+++ Stadium.swift
@@ -72,7 +72,9 @@ var availableRooms: [Int] {
 
 }
 
-// Arena.swift import Foundation
+// Arena.swift 
+
+import Foundation
 
 struct Arena { let stadium: Stadium let roomValue: Int let playersJoined: Int let maxPlayers: Int
 
