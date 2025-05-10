109d33d7d41d6052ed2299dd54b1a914b5000e95
ATMaii <m_maaii@hotmail.com>
2025-05-10 21:57:20 +0700

    Create LoginView.swift

---
+++ LoginView.swift
@@ -0,0 +1,29 @@
+import SwiftUI
+import FacebookLogin
+
+struct LoginView: View {
+    var body: some View {
+        VStack {
+            Text("3Kong1Ciggalate")
+                .font(.largeTitle)
+                .padding()
+
+            Button(action: {
+                loginWithFacebook()
+            }) {
+                Text("Login with Facebook")
+                    .font(.headline)
+                    .foregroundColor(.white)
+                    .frame(width: 220, height: 50)
+                    .background(Color.blue)
+                    .cornerRadius(10)
+            }
+        }
+    }
+
+    func loginWithFacebook() {
+        let manager = LoginManager()
+        manager.logIn(permissions: [.publicProfile, .email], viewController: UIApplication.shared.windows.first?.rootViewController) { result in
+            switch result {
+            case .success(_, _, let token):
+                print("Logged in with token:
