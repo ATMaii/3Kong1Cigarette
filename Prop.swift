40a0d23ec86ea4b059972785d22ac79b14ef3f2f
ATMaii <m_maaii@hotmail.com>
2025-05-13 13:11:59 +0700

    Update Prop.swift

--- Prop.swift
+++ Prop.swift
@@ -1,1 +1,10 @@
-
+let royalFlushSpades = Prop(
+    name: "Royal Flush (Spades)",
+    cards: [
+        Card(rank: .ace, suit: .spades),
+        Card(rank: .king, suit: .spades),
+        Card(rank: .queen, suit: .spades),
+        Card(rank: .jack, suit: .spades),
+        Card(rank: .ten, suit: .spades)
+    ]
+)
