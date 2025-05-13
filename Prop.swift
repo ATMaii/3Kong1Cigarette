18693ea3c974adf43b92100867b81f364ce41485
ATMaii <m_maaii@hotmail.com>
2025-05-13 13:54:30 +0700

    Update Prop.swift

--- Prop.swift
+++ Prop.swift
@@ -1,6 +1,5 @@
-let royalFlush = Prop(
-    name: "RoyalFlush",
-    cards: [
+let royalFlush = Prop = [
+    Prop(name: "RoyalFlush", cards: [
         Card(rank: .ace, suit: .spades),
         Card(rank: .king, suit: .spades),
         Card(rank: .queen, suit: .spades),
@@ -8,7 +7,14 @@ let royalFlush = Prop(
         Card(rank: .ten, suit: .spades)
     ]
 )
-
+    Prop(name: "RoyalFlush", cards: [
+        Card(rank: .ace, suit: .hearts),
+        Card(rank: .king, suit: .hearts),
+        Card(rank: .queen, suit: .hearts),
+        Card(rank: .jack, suit: .hearts),
+        Card(rank: .ten, suit: .hearts)
+    ]
+         
 let straightFlush = Prop(
     name: "A-2-3-4-5 (Spades)",
     cards: [
@@ -107,3 +113,70 @@ let straightFlush = Prop(
         Card(rank: .two, suit: .spades)
     ]
 )
+
+let straightFlushes: [Prop] = [
+    Prop(name: "A-2-3-4-5 (Hearts)", cards: [
+        Card(rank: .ace, suit: .hearts),
+        Card(rank: .two, suit: .hearts),
+        Card(rank: .three, suit: .hearts),
+        Card(rank: .four, suit: .hearts),
+        Card(rank: .five, suit: .hearts)
+    ]),
+    Prop(name: "K-Q-J-10-9 (Hearts)", cards: [
+        Card(rank: .king, suit: .hearts),
+        Card(rank: .queen, suit: .hearts),
+        Card(rank: .jack, suit: .hearts),
+        Card(rank: .ten, suit: .hearts),
+        Card(rank: .nine, suit: .hearts)
+    ]),
+    Prop(name: "Q-J-10-9-8 (Hearts)", cards: [
+        Card(rank: .queen, suit: .hearts),
+        Card(rank: .jack, suit: .hearts),
+        Card(rank: .ten, suit: .hearts),
+        Card(rank: .nine, suit: .hearts),
+        Card(rank: .eight, suit: .hearts)
+    ]),
+    Prop(name: "J-10-9-8-7 (Hearts)", cards: [
+        Card(rank: .jack, suit: .hearts),
+        Card(rank: .ten, suit: .hearts),
+        Card(rank: .nine, suit: .hearts),
+        Card(rank: .eight, suit: .hearts),
+        Card(rank: .seven, suit: .hearts)
+    ]),
+    Prop(name: "10-9-8-7-6 (Hearts)", cards: [
+        Card(rank: .ten, suit: .hearts),
+        Card(rank: .nine, suit: .hearts),
+        Card(rank: .eight, suit: .hearts),
+        Card(rank: .seven, suit: .hearts),
+        Card(rank: .six, suit: .hearts)
+    ]),
+    Prop(name: "9-8-7-6-5 (Hearts)", cards: [
+        Card(rank: .nine, suit: .hearts),
+        Card(rank: .eight, suit: .hearts),
+        Card(rank: .seven, suit: .hearts),
+        Card(rank: .six, suit: .hearts),
+        Card(rank: .five, suit: .hearts)
+    ]),
+    Prop(name: "8-7-6-5-4 (Hearts)", cards: [
+        Card(rank: .eight, suit: .hearts),
+        Card(rank: .seven, suit: .hearts),
+        Card(rank: .six, suit: .hearts),
+        Card(rank: .five, suit: .hearts),
+        Card(rank: .four, suit: .hearts)
+    ]),
+    Prop(name: "7-6-5-4-3 (Hearts)", cards: [
+        Card(rank: .seven, suit: .hearts),
+        Card(rank: .six, suit: .hearts),
+        Card(rank: .five, suit: .hearts),
+        Card(rank: .four, suit: .hearts),
+        Card(rank: .three, suit: .hearts)
+    ]),
+    Prop(name: "6-5-4-3-2 (Hearts)", cards: [
+        Card(rank: .six, suit: .hearts),
+        Card(rank: .five, suit: .hearts),
+        Card(rank: .four, suit: .hearts),
+        Card(rank: .three, suit: .hearts),
+        Card(rank: .two, suit: .hearts)
+    ])
+    
+]
