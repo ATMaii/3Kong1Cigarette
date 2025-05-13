dcbb00ee7c7dc89fd11e276528702557546eee9b
ATMaii <m_maaii@hotmail.com>
2025-05-13 20:24:30 +0700

    Update Prop.swift

--- Prop.swift
+++ Prop.swift
@@ -8,25 +8,32 @@ prop: [
         Card(rank: .ten, suit: .spades)
     ]
 Prop(name: "RoyalFlush"), 
-cards: [
+prop: [
         Card(rank: .ace, suit: .hearts),
         Card(rank: .king, suit: .hearts),
         Card(rank: .queen, suit: .hearts),
         Card(rank: .jack, suit: .hearts),
         Card(rank: .ten, suit: .hearts)
     ]
-
-  Prop(name: "Royal Flush"), 
-cards: [
+Prop(name: "Royal Flush"), 
+prop: [
         Card(rank: .ace, suit: .diamonds),
         Card(rank: .king, suit: .diamonds),
         Card(rank: .queen, suit: .diamonds),
         Card(rank: .jack, suit: .diamonds),
         Card(rank: .ten, suit: .diamonds)
     ]
-                        ]
-let straightFlush : Prop = [Prop
-    (name: "A-2-3-4-5 (Spades)"),
+Prop(name: "Royal Flish")
+prop: [
+    Card(rank: .ace, suit: .clubs),
+    Card(rank: .king, suit: .clubs),
+    Card(rank: .queen, suit: .clubs),
+    Card(rank: .jack, suit: .clubs),
+    Card(rank: .ten, suit: .clubs),
+       ]
+]
+
+let str(name: "A-2-3-4-5 (Spades)"),
 cards: [
         Card(rank: .ace, suit: .spades),
         Card(rank: .two, suit: .spades),
